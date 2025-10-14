46c5a7d9cbbdcb985d9116fe3057fdd502f0314d
Anastasiia Vasilchenko <nastyavasilshenko@mail.ru>
2025-10-14 21:27:02 +0300

    в процессе

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from 'react';
 
 export const App = () => {
-	const [todos, setTodos] = useState([]);
-	const [isLoading, setIsLoading] = useState(false);
-	const [newTodo, setNewTodo] = useState('');
+	const [todos, setTodos] = useState([]); // все дела
+	const [isLoading, setIsLoading] = useState(false); // лоадер
+	const [newTodo, setNewTodo] = useState(''); // текст нового дела
 
+	//загружаем тудус с json-server при загрузке страницы
 	useEffect(() => {
 		setIsLoading(true);
 
@@ -13,13 +14,14 @@ export const App = () => {
 			.then((loadedTodos) => {
 				setTodos(loadedTodos);
 			})
+			.catch((error) => console.log('Ошибка загрузки:', error))
 			.finally(() => setIsLoading(false));
 	}, []);
 
 	return (
 		<div>
 			<h1>Список дел</h1>
-			{isLoading ? <p>Загрузка...</p> : ()}
+			{isLoading ? <p>Загрузка...</p> : todos.map((todo, id) => (<div key={id}>{todo}</div>))}
 		</div>
 	);
 };
