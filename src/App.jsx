cd1dec8ba26fe15e81c40f17af5c0277fc20f7fe
Anastasiia Vasilchenko <nastyavasilshenko@mail.ru>
2025-10-16 20:22:10 +0300

    вынесла CRUD в hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Button } from './components/Button';
+import { useRequestTodos } from './hooks/use-request-todos';
 import buttonStyles from './components/Button.module.css';
 import appStyles from './App.module.css';
 
 export const App = () => {
-	const [todos, setTodos] = useState([]); // все дела
-	const [isLoading, setIsLoading] = useState(false); // лоадер
 	const [newTodo, setNewTodo] = useState(''); // текст нового дела
 
 	const [editingIdTodo, setEditingIdTodo] = useState(null); //id дела которое редактируем
@@ -20,79 +19,11 @@ export const App = () => {
 		setEditingInputTextTodo('');
 	};
 
-	// GET загружаем тудус с json-server при загрузке страницы
-	useEffect(() => {
-		setIsLoading(true);
-
-		fetch('http://localhost:3003/todos')
-			.then((loadedData) => loadedData.json())
-			.then((loadedTodos) => {
-				setTodos(loadedTodos);
-			})
-			.catch((error) => console.log('Ошибка загрузки:', error))
-			.finally(() => setIsLoading(false));
-	}, []);
-
-	// POST создаем тудус
-	const requestAddTodo = (e) => {
-		e.preventDefault();
-
-		const title = newTodo.trim();
-		if (!title) return; // прекращаем выполнение если пустая строка
-
-		fetch('http://localhost:3003/todos', {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json;charset=utf-8' },
-			body: JSON.stringify({
-				text: title,
-				completed: false,
-			}),
-		})
-			.then((rawResponce) => rawResponce.json())
-			.then((responce) => {
-				console.log('Дело добавлено, ответ сервера:', responce);
-				setTodos((prevTodos) => [...prevTodos, responce]); // добавляем задачу
-				setNewTodo('');
-			})
-			.catch((error) => console.log('Ошибка', error));
-	};
-
-	//PUT
-	const requestEditTodo = (id, title) => {
-		const newTitle = title.trim();
-
-		fetch(`http://localhost:3003/todos/${id}`, {
-			method: 'PATCH',
-			headers: { 'Content-Type': 'application/json;charset=utf-8' },
-			body: JSON.stringify({ text: newTitle }),
-		})
-			.then((responce) => responce.json())
-			.then((editedTodo) => {
-				setTodos((prevTodos) =>
-					prevTodos.map((todo) => (todo.id === id ? editedTodo : todo)),
-				);
-				setNewTodo(''); // очищаем инпут
-				cancelEdit();
-			})
-			.catch((err) => console.log('Ошибка', err));
-	};
-
-	//DELETE
-
-	const requestDeleteTodo = (id) => {
-		fetch(`http://localhost:3003/todos/${id}`, {
-			method: 'DELETE',
-		})
-			// .then((rawResponce) => rawResponce.json())
-			.then((responce) => {
-				console.log('Дело удалено, ответ сервера:', responce);
-				setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
-			})
-			.catch((err) => console.log('Ошибка', err));
-	};
+	const { todos, isLoading, requestAddTodo, requestEditTodo, requestDeleteTodo } =
+		useRequestTodos({ newTodo, setNewTodo, cancelEdit });
 
 	return (
-		<div>
+		<div className={appStyles.container}>
 			<h1>Список дел</h1>
 			{isLoading ? (
 				<p>Загрузка...</p>
@@ -153,7 +84,11 @@ export const App = () => {
 						</ul>
 					)}
 					{/* форма */}
-					<form onSubmit={requestAddTodo} className={appStyles.form} id="addTodoForm">
+					<form
+						onSubmit={requestAddTodo}
+						className={appStyles.form}
+						id="addTodoForm"
+					>
 						<input
 							value={newTodo}
 							onChange={(e) => setNewTodo(e.target.value)}
---
+++ src/hooks/use-request-todos.jsx
@@ -0,0 +1,87 @@
+import { useState, useEffect } from 'react';
+
+export const useRequestTodos = ({ newTodo, setNewTodo, cancelEdit }) => {
+	const [todos, setTodos] = useState([]); // все дела
+	const [isLoading, setIsLoading] = useState(false); // лоадер
+
+	// GET загружаем тудус с json-server при загрузке страницы
+		useEffect(() => {
+			setIsLoading(true);
+
+			fetch('http://localhost:3003/todos')
+				.then((loadedData) => loadedData.json())
+				.then((loadedTodos) => {
+					setTodos(loadedTodos);
+				})
+				.catch((error) => console.log('Ошибка загрузки:', error))
+				.finally(() => setIsLoading(false));
+		}, []);
+
+		// POST создаем тудус
+		const requestAddTodo = (e) => {
+			e.preventDefault();
+
+			const title = newTodo.trim();
+			if (!title) return; // прекращаем выполнение если пустая строка
+
+			fetch('http://localhost:3003/todos', {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json;charset=utf-8' },
+				body: JSON.stringify({
+					text: title,
+					completed: false,
+				}),
+			})
+				.then((rawResponce) => rawResponce.json())
+				.then((responce) => {
+					console.log('Дело добавлено, ответ сервера:', responce);
+					setTodos((prevTodos) => [...prevTodos, responce]); // добавляем задачу
+					setNewTodo('');
+				})
+				.catch((error) => console.log('Ошибка', error));
+		};
+
+		//PUT
+		const requestEditTodo = (id, title) => {
+			const newTitle = title.trim();
+
+			fetch(`http://localhost:3003/todos/${id}`, {
+				method: 'PATCH',
+				headers: { 'Content-Type': 'application/json;charset=utf-8' },
+				body: JSON.stringify({ text: newTitle }),
+			})
+				.then((responce) => responce.json())
+				.then((editedTodo) => {
+					setTodos((prevTodos) =>
+						prevTodos.map((todo) => (todo.id === id ? editedTodo : todo)),
+					);
+					setNewTodo(''); // очищаем инпут
+					cancelEdit();
+				})
+				.catch((err) => console.log('Ошибка', err));
+		};
+
+		//DELETE
+
+		const requestDeleteTodo = (id) => {
+			fetch(`http://localhost:3003/todos/${id}`, {
+				method: 'DELETE',
+			})
+				// .then((rawResponce) => rawResponce.json())
+				.then((responce) => {
+					console.log('Дело удалено, ответ сервера:', responce);
+					setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+				})
+				.catch((err) => console.log('Ошибка', err));
+		};
+
+		return ({
+			todos,
+			isLoading,
+			requestAddTodo,
+			requestEditTodo,
+			requestDeleteTodo
+		});
+};
+
+
