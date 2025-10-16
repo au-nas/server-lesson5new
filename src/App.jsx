7fb4eed73023f34e72fef36f3b3ad9bd4f924387
Anastasiia Vasilchenko <nastyavasilshenko@mail.ru>
2025-10-16 19:19:29 +0300

    full CRUD is done

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,16 @@ export const App = () => {
 	const [newTodo, setNewTodo] = useState(''); // текст нового дела
 
 	const [editingIdTodo, setEditingIdTodo] = useState(null); //id дела которое редактируем
-	const [editingTextTodo, setEditingTextTodo] = useState(''); // текст который редактируем
+	const [editingInputTextTodo, setEditingInputTextTodo] = useState(''); // текст который редактируем
+
+	const startEdit = (todo) => {
+		setEditingIdTodo(todo.id);
+		setEditingInputTextTodo(todo.text);
+	};
+	const cancelEdit = () => {
+		setEditingIdTodo(null);
+		setEditingInputTextTodo('');
+	};
 
 	// GET загружаем тудус с json-server при загрузке страницы
 	useEffect(() => {
@@ -43,7 +52,9 @@ export const App = () => {
 			.then((responce) => {
 				console.log('Дело добавлено, ответ сервера:', responce);
 				setTodos((prevTodos) => [...prevTodos, responce]); // добавляем задачу
-			});
+				setNewTodo('');
+			})
+			.catch((error) => console.log('Ошибка', error));
 	};
 
 	//PUT
@@ -53,7 +64,7 @@ export const App = () => {
 		fetch(`http://localhost:3003/todos/${id}`, {
 			method: 'PATCH',
 			headers: { 'Content-Type': 'application/json;charset=utf-8' },
-			body: JSON.stringify({ title: newTitle }),
+			body: JSON.stringify({ text: newTitle }),
 		})
 			.then((responce) => responce.json())
 			.then((editedTodo) => {
@@ -61,6 +72,7 @@ export const App = () => {
 					prevTodos.map((todo) => (todo.id === id ? editedTodo : todo)),
 				);
 				setNewTodo(''); // очищаем инпут
+				cancelEdit();
 			})
 			.catch((err) => console.log('Ошибка', err));
 	};
@@ -92,23 +104,56 @@ export const App = () => {
 						<ul className={appStyles.list}>
 							{todos.map((todo) => (
 								<li key={todo.id} className={appStyles.item}>
-									{todo.text}
-									<Button
-										
-										className={buttonStyles.edit}
-										text="Изменить"
-									/>
-									<Button
-										onClick={() => requestDeleteTodo(todo.id)}
-										className={buttonStyles.delete}
-										text="Удалить"
-									/>
+									{/* {editingIdTodo === todo.id && console.log('Editing id:', todo.id)} */}
+									{editingIdTodo === todo.id ? (
+										<>
+											<input
+												id={`edit-todo-${todo.id}`} // уникальный айди
+												name={`edit-todo-${todo.id}`}
+												value={editingInputTextTodo}
+												onChange={(e) =>
+													setEditingInputTextTodo(
+														e.target.value,
+													)
+												}
+											></input>
+											<Button
+												onClick={() =>
+													requestEditTodo(
+														todo.id,
+														editingInputTextTodo,
+													)
+												}
+												className={buttonStyles.save}
+												text="Сохранить"
+											/>
+											<Button
+												onClick={() => cancelEdit()}
+												className={buttonStyles.cancel}
+												text="Отмена"
+											/>
+										</>
+									) : (
+										<>
+											{todo.text}
+											<Button
+												onClick={() => startEdit(todo)}
+												className={buttonStyles.edit}
+												text="Изменить"
+											/>
+											<Button
+												onClick={() => requestDeleteTodo(todo.id)}
+												className={buttonStyles.delete}
+												text="Удалить"
+											/>
+										</>
+									)}
 								</li>
 							))}
 						</ul>
 					)}
 					{/* форма */}
-					<form onSubmit={requestAddTodo} className={appStyles.form}>
+					<form onSubmit={requestAddTodo} className={appStyles.form} id="addTodoForm">
 						<input
 							value={newTodo}
 							onChange={(e) => setNewTodo(e.target.value)}
